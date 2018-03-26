ff12b195332f21b5141e5c180ae572cc0624337e
GCLe <842017615@qq.com>
2018-03-26 17:10:26 +0800

    添加可视化

--- ABC/core.py
+++ ABC/core.py
@@ -3,6 +3,7 @@ Created on 2018年3月24日
 
 @author: LI
 '''
+import matplotlib.pyplot as plt
 NP=40 #种群的规模，采蜜蜂+观察蜂 
 FoodNumber=NP/2#食物的数量，为采蜜蜂的数量  
 limit=20#限度，超过这个限度没有更新采蜜蜂变成侦查蜂
@@ -47,8 +48,8 @@ NectarSource,EmployedBee,OnLooker,BestSource=baseFunction.initilize(FoodNumber,D
 # print(OnLooker[10].getAllinof())
 # print(BestSource.getcode())
 BestSource=baseFunction.MemorizeBestSource(FoodNumber,NectarSource,BestSource)
-
-print('阶段1')
+process=[]
+# print('阶段1')
 #主要循环
 gen =0
 while gen<maxCycle :
@@ -60,8 +61,18 @@ while gen<maxCycle :
     BestSource=baseFunction.MemorizeBestSource(FoodNumber,NectarSource,BestSource)
    
     f.write(str(BestSource.trueFit)) 
-    f.write('\n') 
+    f.write('\n')
+    process.append(BestSource.trueFit) 
     gen=gen+1
 
 f.close()
 
+from pylab import *  
+mpl.rcParams['font.sans-serif'] = ['SimHei'] #在matplotlib中显示中文
+
+
+x=range(1,maxCycle+1)
+plt.xlabel('迭代次数')
+plt.ylabel('tureFit')
+plt.plot(x,process)
+plt.show()
